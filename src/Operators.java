451681841b95b9df051768b56d2eafda2038dd37
DJAngel973 <angel.djuancho@gmail.com>
2025-06-06 22:18:18 -0500

    Se crea clase Operators para una breve explicación de operadores

---
+++ src/Operators.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Operators {
+    public static void main(String[] args) {
+
+        //Example generate a discount on a value. -Ejemplo generar descuento de un valor.
+        double originalPrice = Double
+                .parseDouble(JOptionPane.showInputDialog("Digite precio del producto: "));
+
+        double discountPercentage = Double
+                .parseDouble(JOptionPane.showInputDialog("Digite el porcentaje de descuento: "));
+
+
+    }
+}
