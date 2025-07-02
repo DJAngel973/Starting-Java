072a99613a76d4326e1e38f72b7d0ed01903cc98
DJAngel973 <angel.djuancho@gmail.com>
2025-07-02 00:20:16 -0500

    feat(MenuConsole):add nested switch in case 1 to calculate two areas

--- src/ExcercisesThirdClass/MenuConsole.java
+++ src/ExcercisesThirdClass/MenuConsole.java
@@ -20,13 +20,104 @@ public class MenuConsole {
         //Variables
         var input = new Scanner(System.in);
         int menuOption; // Take menu opcion
+        boolean exit = false; // exit to finalize menuOpcion
 
-        //
+        // add do-while loop for menu flow
         do{
-            System.out.println("Escoge una opción para interactuar(1, 2, 3 o 4):\n1)Calcular el área de una figura.\n2)Calcular edad futura.\n3)Verificar si un número es primo.\n4)Salir del programa.");
+            System.out.println("""
+                        Escoge una opción para interactuar(1, 2, 3 o 4):
+                        1)Calcular el área de una figura (circulo, rectangulo).
+                        2)Calcular edad futura.
+                        3)Verificar si un número es primo.
+                        4)Salir del programa.
+                        """);
             menuOption = input.nextInt();
-        }while(menuOption < 1 || menuOption  > 4); //If menuOption is less that one or greater that four, the loop repeats
 
+            // switch statement for main menu options
+            switch (menuOption) {
+                case 1:{
+                
+                    // Variable
+                    int subOption;
+                    final var pi =3.14159;
+                    double radio;
+                    double area;
+                    double base;
+                    double height;
+                    boolean exitSubOption = false; // exit to finalize sub-option
+                
+                    // add do-while loop for sub-option
+                    do{
+                        System.out.println("""
+                                    Que área necesitas(1, 2):
+                                    1)Área circulo.
+                                    2)Área rectangulo
+                                    3)Menú principal
+                                    """);
+                        subOption = input.nextInt();
+
+                        switch (subOption) {
+                            case 1: {
+                                // Request the area of the circle
+                                System.out.println("---Área circulo---");
+                                System.out.println("Ingresa el radio del circulo:");
+                                radio = input.nextDouble();
+                                
+                                // Operation to find the area of the circle
+                                area = pi*Math.pow(radio,2);
+
+                                // Print result message
+                                System.out.printf("El área del circulo con radio %.2f es: %.2f\n",radio, area);
+                            }
+                                break;
+                            case 2:{
+                                // Request the base of the rectangle
+                                System.out.println("---Área rectangulo---");
+                                System.out.println("Ingresa la base del rectangulo:");
+                                base = input.nextDouble();
+                                
+                                // Request the height of the rectangle
+                                System.out.println("Ingresa la altura del rectangulo:");
+                                height = input.nextDouble();
+
+                                // Operation to find the arae of the rectangle
+                                area = base*height;
+
+                                // Print result message
+                                System.out.printf("El área del rectangulo es: %.2f\n", area);
+                            }
+                                break;
+                            
+                            case 3:
+                                exitSubOption=true;
+                                System.out.println("----Menú principal----\n");
+                                break;
+                    
+                            default:
+                                System.out.println("Opción invalidad.");
+                                break;
+                        }
+                    }while(!exitSubOption);
+
+                }
+                    break;
+
+                case 2:
+                    break;
+                    
+                case 3:
+                    break;
+                    
+                case 4:
+                    exit=true;
+                    System.out.println("Gracias por participar.");
+                    break;
+                default:
+                System.out.println("Opción invalidad.");
+                    break;
+            }
+        }while(!exit); //If menuOption is less that one or greater that four, the loop repeats
+        
         input.close();
     }
 }
