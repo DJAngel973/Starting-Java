d0d5c2cdff146971ed876094f303767523ec949b
DJAngel973 <angel.djuancho@gmail.com>
2025-07-02 19:13:02 -0500

    feat(ProjectModule1): add menu switch and student registration function

--- src/ExcercisesFourthClass/ProjectModule1.java
+++ src/ExcercisesFourthClass/ProjectModule1.java
@@ -1,5 +1,7 @@
 package ExcercisesFourthClass;
 
+import java.util.Scanner;
+
 /*
 * Module 1 Project: Basic student registration system
 * Project objective
@@ -9,20 +11,58 @@ package ExcercisesFourthClass;
 public class ProjectModule1 {
     public static void main(String[] args) {
 
-        
+        var input = new Scanner(System.in);
+        int optionMenu;
         boolean exit = false;
 
         do {
             System.out.println(" ---- Sistema Registro de Estudiantes ---- ");
-            System.out.println("""
-                    1. Registrar datos de un estudiante
-                    2. Mostrar datos del estudiante actual
-                    3. Calcular promedio de notas del estudiante actual
-                    4. Validar una nota individual
+            System.out.print("""
+                    1. Registrar estudiante
+                    2. Datos estudiante
+                    3. Promedio notas del estudiante
+                    4. Validar nota individual
                     0. Salir
                     Escoge una opción:
                     """);
-        }while(!exit);
+            optionMenu = input.nextInt();
+            input.nextLine();
 
+            switch (optionMenu){
+                case 1:
+                    studentInformation(input);
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 0:
+                    exit = true;
+                    System.out.println("Gracias por tu registro.");
+                    break;
+                default:
+                    System.out.println("Opción invalida.");
+                    break;
+            }
+        }while(!exit);
+    }
+    //
+    public static void studentInformation(Scanner input){
+
+        String name;
+        double firstNote;
+        double secondNote;
+        double thirdNote;
+
+        System.out.print("Nombre: ");
+        name = input.nextLine();
+        System.out.print("Primer nota: ");
+        firstNote = input.nextDouble();
+        System.out.print("Segunda nota: ");
+        secondNote = input.nextDouble();
+        System.out.print("Tercer nota: ");
+        thirdNote = input.nextDouble();
     }
 }
